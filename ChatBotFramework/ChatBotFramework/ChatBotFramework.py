505c295993083b83f160b97eb16f44b9bb678283
khc <khc730@naver.com>
2019-03-20 23:17:59 +0900

    add : echo version

--- ChatBotFramework/ChatBotFramework/ChatBotFramework.py
+++ ChatBotFramework/ChatBotFramework/ChatBotFramework.py
@@ -1,1 +1,13 @@
+import telegram
+import Config
 
+bot = telegram.Bot(token = Config.token)
+lastID = None
+
+while True:
+    updates = bot.getUpdates(lastID)
+    if updates:
+        for u in updates:
+            print(u.message)
+            bot.send_message(u.message.chat_id, text=u.message.text)
+        lastID = updates[-1].update_id + 1
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # ChatBotFramework
 telegram chat bot framework
+
+## Version
+* 1.0 = lastest
+  * Network - Polling
+  * Scheduler - Synchronous
+  * Feature - echo
+
+## Environment
+* Python 3.6
+
+## Prepare
+* pip install python-telegram-bot
